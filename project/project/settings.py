c58c05995327b605a64d73ff5c33bac2b4e8e2b2
RahmaSoltani <129487689+RahmaSoltani@users.noreply.github.com>
2023-08-23 20:36:28 +0100

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
        # 'ENGINE': 'django.db.backends.sqlite3',
       #  'NAME': BASE_DIR / 'db.sqlite3',
       'ENGINE':'django.db.backends.postgresql',
-      'NAME':'e-commerce',
-      'USER':'postgres',
-      'PASSWORD':'rahmarahouma2003',
+      'NAME':'',
+      'USER':'',
+      'PASSWORD':'',
       'HOST':'localhost',
 
     }
